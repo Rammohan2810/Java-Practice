a99ebad873454cda11b16f62b0dc6a408607444f
Rammohan2810 <130207215+Rammohan2810@users.noreply.github.com>
2023-04-09 19:16:49 +0530

    Add files via upload

---
+++ decending.java
@@ -0,0 +1,34 @@
+
+
+public class decending {
+
+public static void main (String[] args){
+
+int a[]= {44,12,22,19,16,26,33};
+   
+int temp;
+      
+  for(int i=0;i<a.length;i++)
+
+      {
+    	  for (int j=i+1;j<a.length;j++)
+
+    	  {
+    		  
+    		  if(a[i]<a[j])   
+    		  {
+    			  
+    			  temp=a[i];
+    			  a[i]=a[j];
+    			  a[j]=temp;
+    			     			  			  
+    		  }
+    		      		  
+    	  }
+    	  
+    	  System.out.println(a[i]);
+   	  
+      }
+      
+        }
+	}
