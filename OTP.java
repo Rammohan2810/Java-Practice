bd2f270f54d5089c83085aae9458d5ab766e8dd9
Rammohan2810 <130207215+Rammohan2810@users.noreply.github.com>
2023-04-09 21:58:54 +0530

    Uploaded OTP generated code

---
+++ OTP.java
@@ -0,0 +1,40 @@
+
+import java.util.* ;
+
+public class OTP {
+
+public static void main (String [] args) {
+
+ String num = "0123456789";
+
+ int len = 6;
+
+char [] otp = new char [ len ] ;
+
+Random r = new Random ();
+
+for (int i=0;i<len;i++)
+
+{
+
+otp [i] = num.charAt(r.nextInt(num.length ()));
+
+}
+
+System.out.println ("OTP - "+ new String(otp));
+
+}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
