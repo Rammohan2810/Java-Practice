5cbaf7f23a5c336294fbcfa565898c70f82a77be
RAMMOHAN R <130207215+Rammohan2810@users.noreply.github.com>
2023-04-19 21:51:40 +0530

    Fibonacci Series

---
+++ Fibonacci.java
@@ -0,0 +1,24 @@
+public class  Fibonacci
+{	 
+public static void main(String[] args) 
+{
+	
+  int n = 10;  
+    for (int i = 0; i < n; i++) 
+    {
+      System.out.print(fibonacci(i) + " ");
+    }
+    }
+				  
+ public static int fibonacci(int n) 
+{
+  if (n == 0 || n == 1) 
+    {
+	return n;
+    } 
+  else 
+    {
+      return fibonacci(n-1) +  fibonacci(n-2);
+    }
+}
+}
