4831fe0a22af7637163f3a3eb7c57150eb904169
RAMMOHAN R <130207215+Rammohan2810@users.noreply.github.com>
2023-04-24 21:39:05 +0530

    Constructor Method

---
+++ customor.java
@@ -0,0 +1,20 @@
+public class customor
+{ 
+   int aadhar_no,pan_no,atm_no;
+   public customor(int  aadhar, int pan, int atm)
+	{
+		aadhar_no = aadhar;
+		pan_no  = pan;
+		atm_no  = atm ;
+	}
+
+	public static void main(String[] args) 
+            {
+		customor ram = new customor (1231,3443,45678);
+		ram.applyloan();
+   	    }
+	private void applyloan() 
+            {
+		System.out.println("customor atm no is "+ atm_no);
+    	    }
+}
