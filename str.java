ad74fb8897aeabf20aab8a54aae84be3dda8e987
RAMMOHAN R <130207215+Rammohan2810@users.noreply.github.com>
2023-04-29 16:11:56 +0530

    String

---
+++ str.java
@@ -0,0 +1,12 @@
+public class str 
+{
+public static void main(String[]args)
+{
+String str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+System.out.println("The length of the str string is: " + str.length());
+
+String txt = "Hello World";
+System.out.println("Upper case is "+txt.toUpperCase());    
+System.out.println("Lower case is "+txt.toLowerCase());
+}
+} 
