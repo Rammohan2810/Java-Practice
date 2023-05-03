04071eef44acbcc5c9e35a160bc58e7891de504f
RAMMOHAN R <130207215+Rammohan2810@users.noreply.github.com>
2023-05-03 20:34:59 +0530

    Post Increment and Pre Increment

---
+++ pre_post.java
@@ -0,0 +1,18 @@
+public class pre_post 
+{
+public static void main(String[] args) 
+{
+        System.out.println("Post Increment");	  	 
+	// a values is incremented to 2 after returning
+        // current value i.e; 1
+		
+	        int a=1;
+	 System.out.println(a++);
+
+	System.out.println("Pre Increment");	  			  
+		int b=1;
+	// j is incremented to 2 and then it's value is
+        // returned
+	System.out.println(++b);		
+}
+}
