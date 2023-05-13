f39023cdcfc8eaecef4ff8e051014f93b5af98cf
RAMMOHAN R <130207215+Rammohan2810@users.noreply.github.com>
2023-05-13 20:17:35 +0530

    Smallest element in the array

---
+++ Small_array.java
@@ -0,0 +1,14 @@
+public class Small_array 
+{  
+public static void main(String[] args) 
+{            
+        int [] arr = new int [] {25, 11, 7, 75, 56};         
+        int min = arr[0];     
+        for (int i = 0; i < arr.length; i++) 
+          {            
+           if(arr[i] <min)  
+               min = arr[i];  
+          }  
+        System.out.println("Smallest element in the array-"+ min);  
+ }  
+}  
