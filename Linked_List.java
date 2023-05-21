b42777f36e429d7b3bf65c3b97e665d052afb325
RAMMOHAN R <130207215+Rammohan2810@users.noreply.github.com>
2023-05-21 18:57:58 +0530

    Linked List Concept

---
+++ Linked_List.java
@@ -0,0 +1,17 @@
+/*Java LinkedList class uses a doubly linked list to store the elements.
+It provides a linked-list data structure */
+
+import java.util.*;
+
+public class Linked_List
+{ 
+public static void main(String[] args) 
+{ 
+    LinkedList<String> cars = new LinkedList<String>();
+    cars.add("Volvo");
+    cars.add("BMW");
+    cars.add("Ford");
+    cars.add("Mazda");
+    System.out.println(cars);
+  } 
+}
