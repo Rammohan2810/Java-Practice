f9a9d9a474b4e14d3a6a146e73e8a34fd03f2548
RAMMOHAN R <130207215+Rammohan2810@users.noreply.github.com>
2023-05-25 17:46:51 +0530

    Show Current Time

---
+++ Time.java
@@ -0,0 +1,10 @@
+import java.time.LocalTime;
+
+public class Time
+{
+public static void main(String[] args) 
+{
+        LocalTime currentTime = LocalTime.now();
+        System.out.println("Current time: " + currentTime);
+}
+}
