df3ae4a15d87c29cf29760c17b87fc61f8200bc6
RAMMOHAN R <130207215+Rammohan2810@users.noreply.github.com>
2023-05-26 11:10:22 +0530

    Show Current Date

---
+++ date.java
@@ -0,0 +1,11 @@
+import java.time.LocalDate;
+
+public class date
+{
+public static void main(String [] args)
+{
+LocalDate date = LocalDate.now();
+System.out.println("Current Date is "+date);
+
+}
+}
