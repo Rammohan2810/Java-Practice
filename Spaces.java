e8299d56e6143a13e11718100ce48df1e4e2199d
RAMMOHAN R <130207215+Rammohan2810@users.noreply.github.com>
2023-05-27 21:45:10 +0530

    Remove All white spaces in the Sentence

---
+++ Spaces.java
@@ -0,0 +1,10 @@
+public class Spaces
+{
+public static void main (String [] args)
+{
+String  Sentance = "This is a string with spaces";
+String  WithoutSpaces = Sentance.replace(" ", "");
+System.out.println("Before the Sentance is "+Sentance);
+System.out.println("After the Sentance is "+WithoutSpaces);
+}
+}
