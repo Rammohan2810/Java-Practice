3a073a46d78fbbf4aa424ed1bfe04a48f209ff3a
RAMMOHAN R <130207215+Rammohan2810@users.noreply.github.com>
2023-05-30 21:54:09 +0530

    Update Method_Overload.java

--- Method_Overload.java
+++ Method_Overload.java
@@ -1,4 +1,4 @@
-/*Method overloading is a feature of Java
+/* Method overloading is a feature of Java
 in which a class has more than one method 
 of the same name and their parameters are different */
 public class Method_Overload
@@ -10,7 +10,7 @@ public static void main(String[] args)
         mul(10,20,30);		
 }
 	private static void mul(int i, int j, int k) {		 
-		int l = i+j+k;		
+		int l = i*j*k;		
 		System.out.println("Multiple of three numbers "+l);		
 	}
 	private static void add(int i, int j) {		 
