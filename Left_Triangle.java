47de3c9996faf2dbd75e01300782512aec1c791d
RAMMOHAN R <130207215+Rammohan2810@users.noreply.github.com>
2023-06-14 19:25:39 +0530

    Left Triangle pattern

---
+++ Left_Triangle.java
@@ -0,0 +1,14 @@
+public class Left_Triangle
+{	 	
+public static void main (String [] args ) 
+{         // Left Triangle Pattern
+ for(int i=1;i<=10;i++)
+ {
+	 for(int j=0;j<i;j++)
+	 {
+		 System.out.print("* ");
+	 }
+	 System.out.println("");
+ }
+}
+} 
