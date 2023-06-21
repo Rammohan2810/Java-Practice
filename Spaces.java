66867ec4b0a7dde5a695ee74cbb84d6373a6faf2
RAMMOHAN R <130207215+Rammohan2810@users.noreply.github.com>
2023-06-21 19:47:29 +0530

    Update Spaces

--- Spaces.java
+++ Spaces.java
@@ -3,7 +3,7 @@ public class Spaces
 public static void main (String [] args)
 {
 String  Sentance = "This is a string with spaces";
-String  WithoutSpaces = Sentance.replace(" ", "");
+String  WithoutSpaces = Sentance.replace(" ","");
 System.out.println("Before the Sentance is "+Sentance);
 System.out.println("After the Sentance is "+WithoutSpaces);
 }
