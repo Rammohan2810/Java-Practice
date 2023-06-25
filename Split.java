bfacef0d532db125656df1f5aa9135ba18e6dd05
RAMMOHAN R <130207215+Rammohan2810@users.noreply.github.com>
2023-06-25 10:00:25 +0530

    Split the Strings

--- Split.java
+++ Split.java
@@ -1,27 +1,17 @@
 public class Split{
 
 public static void main(String [] args){
-
  
 String str= "Rammohan_Java_Developer";
-
-   String[] s = str.split("_");
+String[] s = str.split("_");
 
 String First_split = s[0];
 String Second_split = s[1];
 String Thrid_split = s[2];
 
 System.out.println(First_split);
-
 System.out.println(Second_split);
-
 System.out.println(Thrid_split);
-
-
 }
-
 }
-
- // charAt(), split, sub String,  drim, replace, Indexof()    
-
-//  w3 schools, keypoints
+ // String manipulation methods - charAt(), split, sub String,  drim, replace, Indexof()    
