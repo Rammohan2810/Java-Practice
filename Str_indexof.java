f1dffc33669dfea0bca7707e9daf8a957b3e975c
RAMMOHAN R <130207215+Rammohan2810@users.noreply.github.com>
2023-06-30 20:01:11 +0530

    String Method-Indexof()

---
+++ Str_indexof.java
@@ -0,0 +1,16 @@
+public class Str_indexof
+ {
+public static void main(String [] args)
+    {
+
+    String str = "Hello, World";
+    int index = str.indexOf("World"); 
+
+    System.out.println(index);  // 7
+
+    }
+ }
+
+
+
+
