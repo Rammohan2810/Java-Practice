3e6f88a6c7ffd12fe64f0b4cffe726e029c239a0
RAMMOHAN R <130207215+Rammohan2810@users.noreply.github.com>
2023-07-10 17:52:44 +0530

    Convert Array to  ArrayList

--- Array_1.java
+++ Array_1.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 public class Array_1{
   public static void  main(String[] args) 
-  {
+   {
   String[]  my_array = new String[] {"Python", "JAVA", "PHP", "C#", "C++"};
   ArrayList<String>  list = new ArrayList<String>(Arrays.asList(my_array));
   
